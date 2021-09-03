63ea86c8e9e550ca8dfee479877fb96bf0d99ee7
K547NK <nigel.agwah@gmail.com>
2021-09-03 16:31:33 -0500

    still working

--- src/login.c
+++ src/login.c
@@ -1,4 +1,6 @@
-//input username and password and then use a hash key to mask the password instead of reading it plaintext and then for future use a public private encryption key to prevent reading input from a probable security flaw
+// input username and password and then use a hash key to mask the password instead \\
+// of reading it plaintext and then for future use a public private encryption key  \\
+// to prevent reading input from a probable security flaw    			   \\
  
 #include <stdio.h>
 int main()
