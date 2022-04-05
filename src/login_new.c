37bfc64f474f0f7fb2c0790917a31f2b268025b0
K547NK <nigel.agwah@gmail.com>
2022-04-04 23:26:56 -0500

    Rewriting the login script

---
+++ src/login_new.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int i=0;
+    char user, pass;
+    char ch;
+        printf("Welcome to Logins\n");
+        printf("EMAIL\n: ");
+        scanf(%d, &user);
+        return 0;
+
+}   
+
